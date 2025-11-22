14920e6ce4fb155355ac188010ab47855191ed17
Adi P. <aprakash1353@gmail.com>
2025-11-22 14:59:25 +0000

    Added gravity and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem;
 [DefaultExecutionOrder(-1)]
 public class PlayerController : MonoBehaviour
 {
+    #region Class Variables
+    
     [Header("Components")]
     [SerializeField] private CharacterController characterController;
     [SerializeField] private Camera playerCamera;
@@ -16,69 +18,56 @@ public class PlayerController : MonoBehaviour
     public float drag = 0.1f;
     public float gravity = -9.81f;
     public float jumpSpeed = 8.0f;
+    public float movingThreshold = 0.01f;
 
     [Header("Camera Settings")] 
     public float lookSenseH = 0.1f;
     public float lookSenseV = 0.1f;
     public float lookLimitV = 89f;
     
-    public enum PlayerState
-    {
-        IDLE, 
-        RUNNING, 
-        AIRBORNE
-    }
+    
     private PlayerLocomotionInput _playerLocomotionInput;
     private Vector2 _cameraRotation = Vector2.zero;
     private Vector2 _playerTargetRotation = Vector2.zero;
     private float _verticalVelocity = 0f;
-    private PlayerState _currentState;
+    private PlayerState _playerState;
     
+
+    #endregion
+
+    #region Startup
     private void Awake()
     {
         _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        _playerState = GetComponent<PlayerState>();
     }
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
     void Start()
     {
         
     }
+    #endregion
+
+    #region Update Logic
     
-    // Update is called once per frame
     void Update()
     {
         HandleMovement();
          
-    }
-
-
-    private void LateUpdate()
-    {
-        RotateCamera();
-    }
-
-
-    private void RotateCamera()
-    {
-        _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
-        _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV *  _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
-        
-        _playerTargetRotation.x = transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;  
-        transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
-        
-        playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
-    }
+    }  
 
     private void HandleMovement()
     {
-        HandleJump();
+        UpdateMovementState();
+        HandleVerticalMovement();
         HandleGroundedMovement();
     }
 
 
     private void HandleGroundedMovement()
     {
+        bool isGrounded = _playerState.InGroundedState();
+        
         Vector3 cameraForwardXZ = new Vector3(playerCamera.transform.forward.x, 0f, playerCamera.transform.forward.z).normalized;
         Vector3 cameraRightXZ = new Vector3(playerCamera.transform.right.x, 0f, playerCamera.transform.right.z).normalized;
         Vector3 movementDirection = cameraRightXZ * _playerLocomotionInput.MovementInput.x + cameraForwardXZ * _playerLocomotionInput.MovementInput.y; 
@@ -94,52 +83,79 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    private void HandleJump()
+    private void HandleVerticalMovement()
     {
-        if (_verticalVelocity < 0 && characterController.isGrounded)
+        bool isGrounded = _playerState.InGroundedState();
+
+        if (_verticalVelocity < 0 && isGrounded)
         {
             _verticalVelocity = 0f;
 
-            if (_playerLocomotionInput.JumpPressed && characterController.isGrounded)
-            {
-                _verticalVelocity += Mathf.Sqrt(jumpSpeed * -3f * gravity);
-            }        
         }
-    }
+        
+        _verticalVelocity += gravity * Time.deltaTime;
 
-    #region StateMachine
-
-    private void UpdateState()
-    {
-        switch (_currentState)
+        if (_playerLocomotionInput.JumpPressed && isGrounded)
         {
-            case PlayerState.IDLE:
-                UpdateIdle();
-                break;
-            case PlayerState.RUNNING:
-                UpdateRun();
-                break;
-            case PlayerState.AIRBORNE:
-                UpdateAirborne();
-                break;
+            _verticalVelocity += Mathf.Sqrt(jumpSpeed * -3f * gravity);
         }
     }
 
 
-    private void UpdateIdle()
+    private void UpdateMovementState()
     {
-        ;
+        bool isMovementInput = _playerLocomotionInput.MovementInput != Vector2.zero;
+        bool isHorizontalMovement = HorizontalMovementCheck();
+        bool isGrounded = GroundCheck();
+        
+        PlayerMovementState lateralState = isHorizontalMovement || isMovementInput ? PlayerMovementState.Moving : PlayerMovementState.Idling;
+        _playerState.SetPlayerMovementState(lateralState);
+
+        if (!isGrounded && characterController.velocity.y > 0f)
+        {
+            _playerState.SetPlayerMovementState(PlayerMovementState.Jumping);
+        }
+        else if (!isGrounded && characterController.velocity.y <= 0f)
+        {
+            _playerState.SetPlayerMovementState(PlayerMovementState.Falling);
+        }
+    }
+    #endregion
+
+    #region Late Update Logic
+
+    private void LateUpdate()
+    {
+        RotateCamera();
     }
 
-    private void UpdateRun()
+    
+    private void RotateCamera()
     {
-        ;
+        _cameraRotation.x += lookSenseH * _playerLocomotionInput.LookInput.x;
+        _cameraRotation.y = Mathf.Clamp(_cameraRotation.y - lookSenseV *  _playerLocomotionInput.LookInput.y, -lookLimitV, lookLimitV);
+        
+        _playerTargetRotation.x = transform.eulerAngles.x + lookSenseH * _playerLocomotionInput.LookInput.x;  
+        transform.rotation = Quaternion.Euler(0f, _playerTargetRotation.x, 0f);
+        
+        playerCamera.transform.rotation = Quaternion.Euler(_cameraRotation.y, _cameraRotation.x, 0f);
+    }
+    #endregion
+
+    #region State Checks
+
+    private bool HorizontalMovementCheck()
+    {
+        Vector3 horizontalVelocity = new Vector3(characterController.velocity.x, 0f, characterController.velocity.y);
+        
+        return horizontalVelocity.magnitude > movingThreshold;
     }
 
-    private void UpdateAirborne()
+    private bool GroundCheck()
     {
-        ;
+        return characterController.isGrounded;
     }
 
     #endregion
+
 }
